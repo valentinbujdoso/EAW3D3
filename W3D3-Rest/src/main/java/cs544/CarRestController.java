e64cdc48a3367b26adeae50564d7f6ded10124f6
valentinbujdoso <valentinbujdoso@gmail.com>
2023-06-14 13:09:00 -0500

    Add W3D3-Rest

---
+++ W3D3-Rest/src/main/java/cs544/CarRestController.java
@@ -0,0 +1,41 @@
+package cs544;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
+@RestController
+public class CarRestController {
+    @Autowired
+    private CarService carService;
+
+    @GetMapping("/cars")
+    public List<Car> getAll() {
+        return carService.getAll();
+    }
+
+    @GetMapping("/cars/{id}")
+    public Car get(@PathVariable int id) {
+        return carService.get(id);
+    }
+
+    @PostMapping("/cars")
+    public RedirectView add(@RequestBody Car car) {
+        carService.add(car);
+        return new RedirectView("/cars");
+
+    }
+
+    @PutMapping("/cars")
+    public void update(@RequestBody Car car) {
+        carService.update(car);
+    }
+
+    @DeleteMapping("/cars/delete/{id}")
+    public void delete(@PathVariable int id) {
+        carService.delete(id);
+    }
+
+}
---
+++ W3D3-Rest/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO Car VALUES(NULL, 1999, 'Silver', 'Volvo', 'S80'   );
+-- INSERT INTO Car VALUES(NULL, 1997, 'Red'   , 'Honda', 'Accord');
+
+INSERT INTO cs544.car (color, make, model, year) VALUES ('Silver', 'Volvo', 'S80', 1999);
+INSERT INTO cs544.car (color, make, model, year) VALUES ('Red', 'Honda', 'Accord', 1997);
